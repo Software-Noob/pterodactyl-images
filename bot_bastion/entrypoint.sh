fd14f9079a5dd91f35568f1c7c7b923a92b65fb5
Alex <admin@softwarenoob.com>
2021-03-23 12:22:30 +0200

    Create entrypoint.sh

---
+++ bot_bastion/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
