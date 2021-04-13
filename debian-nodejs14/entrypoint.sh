9f4114f42d3676b1666447263b7dc957975f8cd1
Alex <admin@softwarenoob.com>
2021-04-13 19:40:26 +0300

    attempt at fixing exec env

--- debian-nodejs14/entrypoint.sh
+++ debian-nodejs14/entrypoint.sh
@@ -5,7 +5,11 @@ cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+
+# Print Node.js Version
+node -v
+
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
