dc2e26a201822f1ed050dc00108a61dc8d2dd26e
Alex <admin@softwarenoob.com>
2021-04-13 19:47:56 +0300

    revert exec

--- debian-nodejs15/entrypoint.sh
+++ debian-nodejs15/entrypoint.sh
@@ -4,9 +4,12 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+# Print Node.js Version
+node -v
+
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-exec ${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
