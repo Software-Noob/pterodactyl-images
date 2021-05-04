0031e696cdfbfd1999748cea3ad386303ee3b882
Softwarenoob <admin@softwarenoob.com>
2021-05-04 19:50:27 +0300

    I need sleep.

--- source-sourcemod/entrypoint.sh
+++ source-sourcemod/entrypoint.sh
@@ -21,8 +21,7 @@ cd /home/container/csgo
 # Install Sourcemod/metamod
 if [ -f SOURCE_FLAG ] || [ "${SOURCEMOD}" = 1 ]; then
     echo "Updating Sourcemod/metamod..."
-    curl -sSL -o sourcemod.tar.gz "https://sourcemod.net/latest.php?os=linux&version=1.10" -o metamod.tar.gz "https://sourcemm.net/latest.php?os=linux&version=1.11"
-	curl -sSL -o metamod.tar.gz "https://sourcemm.net/latest.php?os=linux&version=1.11"
+    curl -L -o sourcemod.tar.gz "https://sourcemod.net/latest.php?os=linux&version=1.10" -o metamod.tar.gz "https://sourcemm.net/latest.php?os=linux&version=1.11"
     tar -xf sourcemod.tar.gz
 	tar -xf metamod.tar.gz
     rm sourcemod.tar.gz metamod.tar.gz
