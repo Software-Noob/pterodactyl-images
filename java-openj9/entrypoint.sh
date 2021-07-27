b1b1a15e14e4b88d395f1fc6f3f719c7a2bd0135
Alex <admin@softwarenoob.com>
2021-07-27 18:37:55 +0300

    force rebuild images

--- java-openj9/entrypoint.sh
+++ java-openj9/entrypoint.sh
@@ -7,7 +7,7 @@ java -version
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-# Replace Startup Variables
+# Replace Startup Variables 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
