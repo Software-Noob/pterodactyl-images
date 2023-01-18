41abe981263a06101b7ff09195b3f1fabfd0449d
softwarenoob <admin@softwarenoob.com>
2023-01-18 20:36:09 +0200

    feat(sourcemod): rewrite the whole logic
    Fixes always checking for custom URL even when it's not provided
    Fix backup using INSTALL_PATH instead of addons folder
    Extract hard-coded version into variables
    Do not silence curl
    Add helpers

--- games/source-sourcemod/entrypoint.sh
+++ games/source-sourcemod/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/container || exit 1
-sleep 1
 
+# Terminal Colors variables are not used, but only here for easier reference.
 BOLD_WHITE='\033[1;37m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
@@ -14,6 +14,7 @@ export INTERNAL_IP
 
 # Available Egg Variables
 
+
 ###################################################################################################################
 # SRCDS_APPID - Steam App ID of the game server (required) (ex: 740)                                              #
 # SRCDS_BETAID - Steam App Beta ID of the game server (optional) (ex: 123456)                                     #
@@ -24,12 +25,42 @@ export INTERNAL_IP
 # INSTALL_PATH - Path to install sourcemod/metamod to (optional) (ex: csgo) (default: csgo)                       #
 ###################################################################################################################
 
+SOURCEMOD_LATEST="https://sourcemod.net/latest.php?os=linux&version=1.11"
+METAMOD_LATEST="https://sourcemm.net/latest.php?os=linux&version=1.11"
 
-# Install path defaults to csgo folder. It can be altered with the envrionment variable INSTALL_PATH
-# TODO - Find all default paths for supported games and detect them automatically (tf,dod,css,csgo,etc)
-INSTALL_PATH=${INSTALL_PATH:-csgo}
+print() {
+    echo -e "$1"
+}
 
-# Update Source Server
+print_bold_white() {
+    echo -e "\033[1;37m$1\033[0m"
+}
+
+print_yellow() {
+    echo -e "\033[0;33m$1\033[0m"
+}
+
+print_red() {
+    echo -e "\033[0;31m$1\033[0m"
+}
+
+print_green() {
+    echo -e "\033[0;32m$1\033[0m"
+}
+
+is_valid_url() {
+    local URL="$1"
+    curl --output /dev/null --silent --head --fail "$URL"
+    return $?
+}
+
+download_default_stable() {
+    print_bold_white "Defaulting to and downloading the latest stable SourceMod 1.11 and MetaMod 1.11 releases."
+    curl --location --output sourcemod.tar.gz "$SOURCEMOD_LATEST" --output metamod.tar.gz "$METAMOD_LATEST"
+}
+
+
+# Update Server
 if [[ -n ${SRCDS_APPID} ]]; then
     if [[ -n ${SRCDS_BETAID} ]]; then
         if [[ -n ${SRCDS_BETAPASS} ]]; then
@@ -43,66 +74,65 @@ if [[ -n ${SRCDS_APPID} ]]; then
 fi
 
 # TODO - support specific versions, such as 1.10 6351
+# TODO fix this mess of nested if statements in this whole script. It's not clear and hard to maintain.
 
 # Install SourceMod/Metamod when egg variable SOURCEMOD is 1 or true. Otherwise, skip the whole step and act as normal server.
 if [[ "${SOURCEMOD}" = 1 || "${SOURCEMOD}" == "true" ]]; then
-    mkdir -p /home/container/${INSTALL_PATH}/tmpfiles
-    cd /home/container/${INSTALL_PATH}/tmpfiles || exit 1
+    mkdir -p /home/container/"${INSTALL_PATH}"/tmpfiles
+    cd /home/container/"${INSTALL_PATH}"/tmpfiles || exit 1
 
-    echo -e "${YELLOW}SourceMod variable is set to 1 or true. Installing SourceMod/Metamod...${RESET_COLOR}"
+    print_yellow "SourceMod variable is set to 1 or true. Installing SourceMod/Metamod..."
 
     # Should custom versions be provided, check that they are valid. If not, use latest stable version.
-    if [[ -n ${SM_VERSION} ]]; then
-        SOURCEMOD_SCRAPE=$(curl https://sm.alliedmods.net/smdrop/${SM_VERSION}/sourcemod-latest-linux -s)
+    if [[ -n "${SM_VERSION}" ]]; then
+        SOURCEMOD_SCRAPE=$(curl https://sm.alliedmods.net/smdrop/${SM_VERSION}/sourcemod-latest-linux -sS)
         SOURCEMOD_URL="https://sm.alliedmods.net/smdrop/${SM_VERSION}/${SOURCEMOD_SCRAPE}"
-        if [[ -n ${MM_VERSION} ]]; then
-            METAMOD_SCRAPE=$(curl https://mms.alliedmods.net/mmsdrop/${MM_VERSION}/mmsource-latest-linux -s)
+
+        # scrape the latest version of metamod for the specified sourcemod version and set it as METAMOD_URL
+        if [[ -n "${MM_VERSION}" ]]; then
+            METAMOD_SCRAPE=$(curl https://mms.alliedmods.net/mmsdrop/${MM_VERSION}/mmsource-latest-linux -sS)
             METAMOD_URL="https://mms.alliedmods.net/mmsdrop/${MM_VERSION}/${METAMOD_SCRAPE}"
         fi
     fi
 
-    function is_valid_url() {
-        local URL="$1"
-        curl --output /dev/null --silent --head --fail "$URL"
-        return $?
-    }
-
-    function download_default_stable() {
-        echo -e "Defaulting to the latest stable SM 1.11 and MM 1.11 versions."
-        curl -sSL -o sourcemod.tar.gz "https://sourcemod.net/latest.php?os=linux&version=1.11" -o metamod.tar.gz "https://sourcemm.net/latest.php?os=linux&version=1.11"
-    }
-
-    if is_valid_url "${SOURCEMOD_URL}"; then
-        if is_valid_url "${METAMOD_URL}"; then
-            curl -sSL -o sourcemod.tar.gz "${SOURCEMOD_URL}" -o metamod.tar.gz "${METAMOD_URL}"
+    if [[ -z ${SOURCEMOD_URL} ]]; then
+        download_default_stable
+    else
+        if is_valid_url "${SOURCEMOD_URL}"; then
+            if is_valid_url "${METAMOD_URL}"; then
+                curl --location --output sourcemod.tar.gz "${SOURCEMOD_URL}" --output metamod.tar.gz "${METAMOD_URL}"
+            else
+                print_red "The specified Metamod version: ${MM_VERSION} is not valid."
+                download_default_stable
+            fi
         else
-            echo -e "${RED}The specified Metamod version: ${MM_VERSION} is not valid.${RESET_COLOR}"
+            print_red "The specified SourceMod version: ${SM_VERSION} is not valid."
             download_default_stable
         fi
-    else
-        echo -e "${RED}The specified SourceMod version: ${SM_VERSION} is not valid.${RESET_COLOR}"
-        download_default_stable
     fi
 
     # Extract SourceMod and Metamod
-    echo "Extracting MM files"
-    tar -xf metamod.tar.gz --directory /home/container/${INSTALL_PATH}
+    print_bold_white "Extracting MetaMod files"
+    tar -xf metamod.tar.gz --directory /home/container/"${INSTALL_PATH}"
     if [[ ! -f /home/container/${INSTALL_PATH}/cfg/sourcemod/sourcemod.cfg ]]; then
-        echo "Existing sourcemod.cfg not found. Performing full SourceMod install."
-        tar -xf sourcemod.tar.gz --directory /home/container/${INSTALL_PATH}
+        print_bold_white "Existing sourcemod.cfg not found. Extracting all SourceMod files."
+        tar -xf sourcemod.tar.gz --directory /home/container/"${INSTALL_PATH}"
     else
-        echo "Extracting SM files"
+        print_bold_white "Existing sourcemod.cfg found. Extracting only the required files for an update\n"
         tar -xf sourcemod.tar.gz
         cp -R addons/sourcemod/{bin,extensions,gamedata,translations} ../addons/sourcemod/
     fi
     rm -rf "/home/container/${INSTALL_PATH}/tmpfiles"
-    echo -e "${GREEN}Sourcemod/metamod has been installed!${RESET_COLOR}"
+    print_green "SourceMod and Metamod has been installed!\n"
 else
-    echo -e "${YELLOW}SourceMod variable is set to false or 0. Skipping SourceMod/Metamod update...${RESET_COLOR}"
+    print_yellow "SOURCEMOD variable is set to false or 0. Skipping SourceMod and MetaMod installation..."
 
-    if [[ -d /home/container/${INSTALL_PATH}/addons/sourcemod ]]; then
-        echo -e "${YELLOW}Sourcemod folder detected. Renaming the folder to disable it.${RESET_COLOR}"
-        mv "/home/container/${INSTALL_PATH}/addons/sourcemod" "/home/container/${INSTALL_PATH}/addons/sourcemod_disabled_$(date +%Y-%m-%d-%H:%M)"
+    if [[ -d /home/container/${INSTALL_PATH}/addons ]]; then
+        print_yellow "Existing SourceMod install folder detected."
+        print_yellow "Renaming the folder /home/container/${INSTALL_PATH}/addons and ${INSTALL_PATH}/cfg/sourcemod to backup and disable it.\n"
+        mv "/home/container/${INSTALL_PATH}/addons" "/home/container/${INSTALL_PATH}/addons_disabled_$(date +%Y-%m-%d-%H:%M)"
+        mkdir -p "/home/container/${INSTALL_PATH}/cfg_disabled"
+        mv "/home/container/${INSTALL_PATH}/cfg/sourcemod" "/home/container/${INSTALL_PATH}/cfg_disabled/sourcemod_disabled_$(date +%Y-%m-%d-%H:%M)"
     fi
 fi
 
@@ -110,10 +140,10 @@ cd /home/container || exit 1
 
 # Replace Startup Variables
 # shellcheck disable=SC2086
-MODIFIED_STARTUP=$(eval echo $(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
 
 # Display the parsed startup string we're going to execute.
-echo -e "${YELLOW}[Startup Command]: ${BOLD_WHITE} ${MODIFIED_STARTUP} ${RESET_COLOR}"
+print_yellow "[Startup Command]: ${BOLD_WHITE} ${MODIFIED_STARTUP}\n"
 
 # Run the Server
 # shellcheck disable=SC2086
